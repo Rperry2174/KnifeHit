8472538217220e1fba59f3f19279ef4b8ad48c08
Ryan Perry <Rperry2174@gmail.com>
2018-09-26 00:33:34 -0700

    Adds dockedKnife

---
+++ Assets/Scripts/MANAGERS/dockedKnifeCtrl.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dockedKnifeCtrl : MonoBehaviour {
+
+	void Start ()
+	{
+	}
+
+	void Update ()
+	{
+	}
+
+	void OnCollisionEnter2D(Collision2D col)
+  {
+  }
+}
--- Assets/Scripts/MANAGERS/wheelCtrl.cs
+++ Assets/Scripts/MANAGERS/wheelCtrl.cs
@@ -6,48 +6,48 @@ public class wheelCtrl : MonoBehaviour {
 
 	public float rotateSpeedMultiplier = 4;
 	public CircleCollider2D circleCollider2D;
-	public GameObject knife;
+	public GameObject dockedKnife;
 
-	void Start () 
+	void Start ()
 	{
 		circleCollider2D = gameObject.GetComponent<CircleCollider2D>();
 
-		//gameObject.transform.parent.
-		//Debug.Log("radius: " + circleCollider2D.radius);
-		//Debug.Log("offset: " + circleCollider2D.offset);
+		float angle = 50.0f;
+		float x = circleCollider2D.radius * Mathf.Cos(angle);
+		float y = circleCollider2D.radius * Mathf.Sin(angle);
 
-		GameObject k0 = InstantiateDockedKnife(new Vector2(1.8f, circleCollider2D.radius));
-		GameObject k1 = InstantiateDockedKnife(new Vector2(-1.8f, circleCollider2D.radius));
-		GameObject k2 = InstantiateDockedKnife(new Vector2(1.8f, -circleCollider2D.radius));
-		GameObject k3 = InstantiateDockedKnife(new Vector2(-1.8f, -circleCollider2D.radius));
-	}   
-    
-	void Update () 
+		Vector2 angleMadeKnife = new Vector2(x, y);
+		GameObject k0 = InstantiateDockedKnife(angleMadeKnife);
+	}
+
+	void Update ()
 	{
 		RotateRight();
 	}
-    
+
 	void RotateRight()
-    {
+  {
 		Vector3 theRotation = new Vector3(0, 0, -1 * rotateSpeedMultiplier);
 		transform.Rotate(theRotation);
-    }
-    
-	GameObject InstantiateDockedKnife(Vector2 knifeStartPos)
-    {
-		GameObject k = Instantiate(knife, knifeStartPos, Quaternion.Euler(0.0f, 0.0f, 90.0f));
+  }
+
+	GameObject InstantiateDockedKnife(Vector2 dockedKnifeStartPos)
+	{
+		// If you need to access knife height
+		// float knifeHeight = dockedKnife.GetComponent<BoxCollider2D>().size.y;
+		// dockedKnifeStartPos.y = dockedKnifeStartPos.y + knifeHeight;
+
+		GameObject k = Instantiate(dockedKnife, dockedKnifeStartPos, Quaternion.Euler(0.0f, 0.0f, 90.0f));
 
 		k.name = "docked_knife_0";
-        k.transform.parent = transform;
-        
-        //Find angle between bottom of circle and whereve knife is on circle
+    k.transform.parent = transform;
+
+    // Find angle between bottom of circle and whereve knife is on circle
 		Vector2 circleColliderRight = new Vector2(circleCollider2D.radius, 0.0f);
-		int angleMultiplier = knifeStartPos.y >= 0 ? 1 : -1;
-		float angle = Vector2.Angle(circleColliderRight, knifeStartPos);
+		int angleMultiplier = dockedKnifeStartPos.y >= 0 ? 1 : -1;
+		float angle = Vector2.Angle(circleColliderRight, dockedKnifeStartPos);
 
 		k.transform.Rotate(new Vector3(0, 0, angle * angleMultiplier));
-        k.GetComponent<knifeCtrl>().isDocked = true;
-
-        return k;
-    }
+    return k;
+  }
 }
