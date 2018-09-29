64a393b8ee30d46cd4bf3080e4a65c6fd9986de2
Ryan Perry <Rperry2174@gmail.com>
2018-09-29 00:02:56 -0700

    Add spawning docked knives from array

--- Assets/Scripts/MANAGERS/wheelCtrl.cs
+++ Assets/Scripts/MANAGERS/wheelCtrl.cs
@@ -4,20 +4,46 @@ using UnityEngine;
 
 public class wheelCtrl : MonoBehaviour {
 
+	private float[] angles = new float[] {0.0f, 120.0f, 240.0f};
 	public float rotateSpeedMultiplier = 4;
 	public CircleCollider2D circleCollider2D;
 	public GameObject dockedKnife;
+	public Vector2 circleColliderRight;
+
 
 	void Start ()
 	{
 		circleCollider2D = gameObject.GetComponent<CircleCollider2D>();
+		circleColliderRight = new Vector2(circleCollider2D.radius, 0.0f);
 
-		float angle = 50.0f;
-		float x = circleCollider2D.radius * Mathf.Cos(angle);
-		float y = circleCollider2D.radius * Mathf.Sin(angle);
+		foreach (float angle in angles){
+			GameObject k0 = InstantiateDockedKnife(angle);
+		}
+	}
 
-		Vector2 angleMadeKnife = new Vector2(x, y);
-		GameObject k0 = InstantiateDockedKnife(angleMadeKnife);
+	Vector2 angleToVector2 (float angle)
+	{
+		int xDirection = (angle <= 90.0f || angle >= 270.0f) ? 1 : -1;
+		int yDirection = angle < 180.0f ? 1 : -1;
+
+		float x = circleCollider2D.radius * Mathf.Cos(angle) * xDirection;
+		float y = circleCollider2D.radius * Mathf.Sin(angle) * yDirection;
+
+		return new Vector2(x, y);
+	}
+
+	float Vector2ToAngle (Vector2 point)
+	{
+		return Vector2.Angle(circleColliderRight, point);
+	}
+
+	float addDockedKnifeToWheel (float angle) {
+		float totalAngle = gameObject.transform.rotation.z + angle;
+		if (totalAngle >= 360.0f) {
+			return totalAngle - 360.0f;
+		} else {
+			return totalAngle;
+		}
 	}
 
 	void Update ()
@@ -31,8 +57,9 @@ public class wheelCtrl : MonoBehaviour {
 		transform.Rotate(theRotation);
   }
 
-	GameObject InstantiateDockedKnife(Vector2 dockedKnifeStartPos)
+	GameObject InstantiateDockedKnife(float angle)
 	{
+		Vector2 dockedKnifeStartPos = angleToVector2(angle);
 		// If you need to access knife height
 		// float knifeHeight = dockedKnife.GetComponent<BoxCollider2D>().size.y;
 		// dockedKnifeStartPos.y = dockedKnifeStartPos.y + knifeHeight;
@@ -43,11 +70,10 @@ public class wheelCtrl : MonoBehaviour {
     k.transform.parent = transform;
 
     // Find angle between bottom of circle and whereve knife is on circle
-		Vector2 circleColliderRight = new Vector2(circleCollider2D.radius, 0.0f);
 		int angleMultiplier = dockedKnifeStartPos.y >= 0 ? 1 : -1;
-		float angle = Vector2.Angle(circleColliderRight, dockedKnifeStartPos);
+		float rotation = Vector2ToAngle(dockedKnifeStartPos);
 
-		k.transform.Rotate(new Vector3(0, 0, angle * angleMultiplier));
+		k.transform.Rotate(new Vector3(0, 0, rotation * angleMultiplier));
     return k;
   }
 }
