17461336b76c655d96c48ed2ca8d558f924ccff9
Ryan Perry <Rperry2174@gmail.com>
2018-09-29 18:57:14 -0700

    Add tracker of how many knives are left

--- Assets/Scripts/MANAGERS/gameBoardCtrl.cs
+++ Assets/Scripts/MANAGERS/gameBoardCtrl.cs
@@ -1,66 +1,106 @@
-﻿using System.Collections;
+﻿
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class gameBoardCtrl : MonoBehaviour {
-	public int numKnives = 12;
-	public int currentKnife = 0;
-	public List<GameObject> knives = new List<GameObject>();
+public class gameBoardCtrl : MonoBehaviour
+{
+    private Vector2 defaultKnifeStartPos = new Vector2(0.0f, -5.263408f);
+    private Vector2 defaultBlackKnifeStartPos = new Vector2(-7.0f, 0.0f);
+    private static int BLACK_KNIFE_SPACING = 1;
 
-  public GameObject knife;
-  public int gameIsWon = 0;
-	private Vector2 defaultKnifeStartPos = new Vector2(0.0f, -5.263408f);
+    public int currentKnife = 0;
+    public int numKnives = 12;
+    public int gameIsWon = 0;
 
+    public GameObject blackKnife;
+    public GameObject knife;
+    public List < GameObject > knives = new List < GameObject > ();
+    public List < GameObject > blackKnives = new List < GameObject > ();
 
+    void Start()
+    {
+        Vector2 adjustedBlackKnifePosition = defaultBlackKnifeStartPos;
 
-	void Start () {
-		for (int i = 0; i < numKnives; i++) {
-			knives.Add(knife);
-		}
-		DequeueKnife();
-	}
+        for (int i = 0; i < numKnives; i++)
+        {
+            adjustedBlackKnifePosition.y -= BLACK_KNIFE_SPACING;
+            knives.Add(knife);
+            GameObject bk = Instantiate(blackKnife, adjustedBlackKnifePosition, transform.rotation);
+            bk.transform.parent = gameObject.transform;
 
-	void Update () {
-	}
+            blackKnives.Add(bk);
+        }
+        DequeueKnife();
+    }
 
-	// Note: This only occurs when knife hits "knife_hit_wheel"
-	public void DequeueKnife()
-	{
-		if(knives.Count > 0)
-		{
-			GameObject k = knives[0];
-			knives.RemoveAt(0);
-			InstantiateKnife(k);
-			currentKnife++;
-		}
-		else
-		{
-			SetWinLossStatus(1);
-		}
-	}
+    void Update()
+    {}
 
-	public void CheckWinLossStatus() {
+    // Note: This only occurs when knife hits "knife_hit_wheel"
+    public void DequeueKnife()
+    {
+        if (knives.Count > 0)
+        {
+            GameObject k = knives[0];
+            GameObject bk = blackKnives[0];
 
-	}
+            knives.RemoveAt(0);
+            blackKnives.RemoveAt(0);
 
-	public void SetWinLossStatus(int i) {
-		gameIsWon = i;
-	}
+            InstantiateKnife(k);
+            Destroy(bk);
+            
+            currentKnife++;
+        }
+        else
+        {
+            SetWinLossStatus(1);
+        }
+    }
 
-	GameObject InstantiateKnife(GameObject listKnife, Vector2? knifeStartPos = null, bool? isDocked = false) {
-		Vector2 startPosition;
+    public void CheckWinLossStatus()
+    {
 
-		if (knifeStartPos == null) {
-			startPosition = defaultKnifeStartPos;
-		} else {
-			startPosition = (Vector2) knifeStartPos;
-		}
+    }
 
-		GameObject k = Instantiate(listKnife, defaultKnifeStartPos, transform.rotation);
-		k.name = "knife_0" + currentKnife;
-		k.transform.parent = gameObject.transform;
-		k.GetComponent<knifeCtrl>().isDocked = (bool) isDocked;
+    public void SetWinLossStatus(int i)
+    {
+        gameIsWon = i;
+    }
 
-		return k;
-	}
+    GameObject InstantiateKnife(GameObject listKnife, Vector2 ? knifeStartPos = null, bool ? isDocked = false)
+    {
+        Vector2 startPosition;
+
+        if (knifeStartPos == null)
+        {
+            startPosition = defaultKnifeStartPos;
+        }
+        else
+        {
+            startPosition = (Vector2) knifeStartPos;
+        }
+
+        GameObject k = Instantiate(listKnife, defaultKnifeStartPos, transform.rotation);
+        k.name = "knife_0" + currentKnife;
+        k.transform.parent = gameObject.transform;
+        k.GetComponent < knifeCtrl > ().isDocked = (bool) isDocked;
+
+        return k;
+    }
+
+    // GameObject InstantiateBlackKnives()
+    // {
+    //   Vector2 adjustedPosition;
+    //   for(int i = (numKnives - 1); i >= 0; i--)
+    //   {
+    //     GameObject bk = blackKnives[i];
+    //     blackKnives.RemoveAt(i);
+    //     adjustedPosition = defaultBlackKnifeStartPos;
+    //     adjustedPosition.x -= i;
+    //     GameObject blackKnife = Instantiate(bk, (defaultBlackKnifeStartPos - i) , transform.rotation);
+    //     blackKnife.transform.parent = gameObject.transform;
+    //   }
+    // }
 }
--- Assets/Scripts/MANAGERS/wheelCtrl.cs
+++ Assets/Scripts/MANAGERS/wheelCtrl.cs
@@ -1,79 +1,91 @@
-﻿using System.Collections;
+﻿
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class wheelCtrl : MonoBehaviour {
+public class wheelCtrl : MonoBehaviour
+{
 
-	private float[] angles = new float[] {0.0f, 120.0f, 240.0f};
-	public float rotateSpeedMultiplier = 4;
-	public CircleCollider2D circleCollider2D;
-	public GameObject dockedKnife;
-	public Vector2 circleColliderRight;
+    private float[] angles = new float[]
+    {
+        0.0f,
+        120.0f,
+        240.0f
+    };
+    public float rotateSpeedMultiplier = 4;
+    public CircleCollider2D circleCollider2D;
+    public GameObject dockedKnife;
+    public Vector2 circleColliderRight;
 
 
-	void Start ()
-	{
-		circleCollider2D = gameObject.GetComponent<CircleCollider2D>();
-		circleColliderRight = new Vector2(circleCollider2D.radius, 0.0f);
+    void Start()
+    {
+        circleCollider2D = gameObject.GetComponent < CircleCollider2D > ();
+        circleColliderRight = new Vector2(circleCollider2D.radius, 0.0f);
 
-		foreach (float angle in angles){
-			InstantiateDockedKnife(angle);
-		}
-	}
+        foreach(float angle in angles)
+        {
+            InstantiateDockedKnife(angle);
+        }
+    }
 
-	Vector2 angleToVector2 (float angle)
-	{
-		int xDirection = (angle <= 90.0f || angle >= 270.0f) ? 1 : -1;
-		int yDirection = angle < 180.0f ? 1 : -1;
+    Vector2 angleToVector2(float angle)
+    {
+        int xDirection = (angle <= 90.0f || angle >= 270.0f) ? 1 : -1;
+        int yDirection = angle < 180.0f ? 1 : -1;
 
-		float x = circleCollider2D.radius * Mathf.Cos(angle) * xDirection;
-		float y = circleCollider2D.radius * Mathf.Sin(angle) * yDirection;
+        float x = circleCollider2D.radius * Mathf.Cos(angle) * xDirection;
+        float y = circleCollider2D.radius * Mathf.Sin(angle) * yDirection;
 
-		return new Vector2(x, y);
-	}
+        return new Vector2(x, y);
+    }
 
-	float Vector2ToAngle (Vector2 point)
-	{
-		return Vector2.Angle(circleColliderRight, point);
-	}
+    float Vector2ToAngle(Vector2 point)
+    {
+        return Vector2.Angle(circleColliderRight, point);
+    }
 
-	float addDockedKnifeToWheel (float angle) {
-		float totalAngle = gameObject.transform.rotation.z + angle;
-		if (totalAngle >= 360.0f) {
-			return totalAngle - 360.0f;
-		} else {
-			return totalAngle;
-		}
-	}
+    float addDockedKnifeToWheel(float angle)
+    {
+        float totalAngle = gameObject.transform.rotation.z + angle;
+        if (totalAngle >= 360.0f)
+        {
+            return totalAngle - 360.0f;
+        }
+        else
+        {
+            return totalAngle;
+        }
+    }
 
-	void Update ()
-	{
-		RotateRight();
-	}
+    void Update()
+    {
+        RotateRight();
+    }
 
-	void RotateRight()
-  {
-		Vector3 theRotation = new Vector3(0, 0, -1 * rotateSpeedMultiplier);
-		transform.Rotate(theRotation);
-  }
+    void RotateRight()
+    {
+        Vector3 theRotation = new Vector3(0, 0, -1 * rotateSpeedMultiplier);
+        transform.Rotate(theRotation);
+    }
 
-	GameObject InstantiateDockedKnife(float angle)
-	{
-		Vector2 dockedKnifeStartPos = angleToVector2(angle);
-		// If you need to access knife height
-		// float knifeHeight = dockedKnife.GetComponent<BoxCollider2D>().size.y;
-		// dockedKnifeStartPos.y = dockedKnifeStartPos.y + knifeHeight;
+    GameObject InstantiateDockedKnife(float angle)
+    {
+        Vector2 dockedKnifeStartPos = angleToVector2(angle);
+        // If you need to access knife height
+        // float knifeHeight = dockedKnife.GetComponent<BoxCollider2D>().size.y;
+        // dockedKnifeStartPos.y = dockedKnifeStartPos.y + knifeHeight;
 
-		GameObject k = Instantiate(dockedKnife, dockedKnifeStartPos, Quaternion.Euler(0.0f, 0.0f, 90.0f));
-		k.name = "docked_knife_0";
-    k.transform.parent = transform;
-		k.transform.position += gameObject.transform.position;
+        GameObject k = Instantiate(dockedKnife, dockedKnifeStartPos, Quaternion.Euler(0.0f, 0.0f, 90.0f));
+        k.name = "docked_knife_0";
+        k.transform.parent = transform;
+        k.transform.position += gameObject.transform.position;
 
-    // Find angle between bottom of circle and whereve knife is on circle
-		int angleMultiplier = dockedKnifeStartPos.y >= 0 ? 1 : -1;
-		float rotation = Vector2ToAngle(dockedKnifeStartPos);
+        // Find angle between bottom of circle and whereve knife is on circle
+        int angleMultiplier = dockedKnifeStartPos.y >= 0 ? 1 : -1;
+        float rotation = Vector2ToAngle(dockedKnifeStartPos);
 
-		k.transform.Rotate(new Vector3(0, 0, rotation * angleMultiplier));
-    return k;
-  }
+        k.transform.Rotate(new Vector3(0, 0, rotation * angleMultiplier));
+        return k;
+    }
 }
